65bf5add474921db45004b86f50950cb12240a52
yashesh72 <65954020+yashesh72@users.noreply.github.com>
2022-11-11 15:36:33 -0500

    Delete Automatic boarding control at airport/Project Final directory

--- Automatic boarding control at airport/Project Final/GoBoard/alogout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION["email"]);
-   unset($_SESSION["pwd"]);
-   session_destroy();
-?>
-<script type="text/javascript">
-	window.location="alogin.php";
-</script>
